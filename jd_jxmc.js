9351961cb0fb67f4fe36da2c3fc93fbfdd4a0305
leastfrog <41647217+leastfrog@users.noreply.github.com>
2022-01-05 20:35:04 +0800

    05

--- jd_jxmc.js
+++ jd_jxmc.js
@@ -34,6 +34,13 @@ $.inviteCodeList = [];
 let cookiesArr = [];
 let UA, token, UAInfo = {}
 $.appId = 10028;
+function oc(fn, defaultVal) {//optioanl chaining
+  try {
+    return fn()
+  } catch (e) {
+    return undefined
+  }
+}
 let cardinfo = {
   "16": "小黄鸡",
   "17": "辣子鸡",
@@ -103,7 +110,6 @@ if ($.isNode()) {
     await pasture();
     await $.wait(2000);
   }
-  $.res = []
   await shareCodesFormat()
   for (let i = 0; i < cookiesArr.length; i++) {
     $.cookie = cookiesArr[i];
@@ -152,7 +158,7 @@ async function pasture() {
         console.log(`\n温馨提示：${$.UserName} 请先手动完成【新手指导任务】再运行脚本再运行脚本\n`);
         return;
       }
-      $.currentStep = $.homeInfo?.finishedtaskId
+      $.currentStep = oc(() => $.homeInfo.finishedtaskId)
       console.log(`打印新手流程进度：当前进度：${$.currentStep}，下一流程：${$.homeInfo.maintaskId}`)
       if ($.homeInfo.maintaskId !== "pause" || isNew($.currentStep)) {
         console.log(`开始初始化`)
@@ -161,7 +167,7 @@ async function pasture() {
         for (let i = 0; i < 20; i++) {
           if ($.DoMainTask.maintaskId !== "pause") {
             await $.wait(2000)
-            $.currentStep = $.DoMainTask?.finishedtaskId
+            $.currentStep = oc(() => $.DoMainTask.finishedtaskId)
             $.step = $.DoMainTask.maintaskId
             await takeGetRequest('DoMainTask');
           } else if (isNew($.currentStep)) {
@@ -189,7 +195,7 @@ async function pasture() {
           }
         }
       }
-      const petNum = ($.homeInfo?.petinfo || []).length
+      const petNum = (oc(() => $.homeInfo.petinfo) || []).length
       await takeGetRequest('GetCardInfo');
       if ($.GetCardInfo && $.GetCardInfo.cardinfo) {
         let msg = '';
@@ -645,7 +651,7 @@ function dealReturn(type, data) {
         $.homeInfo = data.data;
         $.activeid = $.homeInfo.activeid
         $.activekey = $.homeInfo.activekey || null
-        $.coins = $.homeInfo?.coins || 0;
+        $.coins = oc(() => $.homeInfo.coins) || 0;
         if ($.homeInfo.giftcabbagevalue) {
           console.log(`登陆获得白菜：${$.homeInfo.giftcabbagevalue} 颗`);
         }
@@ -905,17 +911,12 @@ function shareCodesFormat() {
 }
 function readShareCode() {
   return new Promise(async resolve => {
-    $.get({url: ``, timeout: 30 * 100}, (err, resp, data) => {
+    $.get({url: ``, timeout: 30 * 1000}, (err, resp, data) => {
       try {
         if (err) {
           console.log(JSON.stringify(err))
-          console.log(`${$.name} readShareCode 已清除 sgh`)
-        } else {
-          if (data) {
-            console.log(`\n注意 检查 随机取20个码`)
-            //data = JSON.parse(data);
-          }
-        }
+          console.log(`${$.name} readShareCode clean by sgh`)
+        } 
       } catch (e) {
         $.logErr(e, resp)
       } finally {
