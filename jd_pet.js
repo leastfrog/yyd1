29eac8bdcdf99452adceaa081acd218d398f2e17
leastfrog <41647217+leastfrog@users.noreply.github.com>
2022-01-12 21:48:01 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -487,7 +487,9 @@ function shareCodesFormat() {
     } else if ((shareCodes.length - 1)) {
       console.log(`由于您第${$.index}个京东账号未提供shareCode,将采纳本脚本自带的助力码\n`)
       const tempIndex = $.index > shareCodes.length ? (shareCodes.length - 1) : ($.index - 1);
-      newShareCodes = shareCodes[tempIndex].split('@');
+      if (tempIndex) {
+      	newShareCodes = shareCodes[tempIndex].split('@');
+      }
     }
     //因好友助力功能下线。故暂时屏蔽
     //const readShareCodeRes = await readShareCode();
